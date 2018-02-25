c76bb003dd2fe2dc2b05445098adb77033c75423
Josh Griffith <josh@starshipconsulting.com>
2018-02-25 13:17:14 -0500

    Partial restore

---
+++ AI/Simulation.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Starship.Core.AI {
+    public class Simulation {
+    }
+}
---
+++ AI/StateContainer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Starship.Core.AI {
+    public class StateContainer {
+
+        public Dictionary<string, object> State { get; set; }
+    }
+}
